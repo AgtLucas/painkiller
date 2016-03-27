5809f442ebfd0b0ea310834c3aa1106bd881d857
Lucas <AgtLucas@users.noreply.github.com>
2016-03-27 13:36:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-'use strict';
+'use strict'
 
-var expect = require('expect');
+var expect = require('expect')
 
 function counter(state, action) {
   if (action.type === 'INCREMENT') {
-    return state + 1;
+    return state + 1
   } else if (action.type === 'DECREMENT') {
-    return state - 1;
+    return state - 1
   }
 }
 
 expect(
   counter(0, { type: 'INCREMENT' })
-).toEqual(1);
+).toEqual(1)
 
 expect(
   counter(1, { type: 'INCREMENT' })
-).toEqual(2);
+).toEqual(2)
 
 expect(
   counter(2, { type: 'DECREMENT' })
-).toEqual(1);
+).toEqual(1)
 
 expect(
   counter(1, { type: 'DECREMENT' })
-).toEqual(0);
+).toEqual(0)
